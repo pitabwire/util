44414c758388fc6e322f45bcdab1a8136169cd7f
Peter Bwire <bwire517@gmail.com>
2025-07-31 12:07:05 +0300

    add functionality to log closer errors simplifying defer errors

---
+++ closer.go
@@ -0,0 +1,17 @@
+package util
+
+import (
+	"context"
+	"io"
+)
+
+// CloseAndLogOnError Closes io.Closer and logs the error if any with the messages supplied
+func CloseAndLogOnError(ctx context.Context, closer io.Closer, message ...string) {
+	if closer == nil {
+		return
+	}
+	err := closer.Close()
+	if err != nil && len(message) > 0 {
+		Log(ctx).WithError(err).Error(message[0])
+	}
+}
